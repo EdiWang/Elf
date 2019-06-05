dc34e0cda93a7ae46cb52e3bb1737c7063f81891
Edi Wang <Edi.Wang@outlook.com>
2019-06-05 16:37:49 +0800

    Add SetupHelper but not use it

---
+++ src/LinkForwarder/Services/SetupHelper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Dapper;
+using Edi.Practice.RequestResponseModel;
+
+namespace LinkForwarder.Services
+{
+    public class SetupHelper
+    {
+        public string DatabaseConnectionString { get; set; }
+
+        public SetupHelper(string databaseConnectionString)
+        {
+            if (string.IsNullOrWhiteSpace(databaseConnectionString))
+            {
+                throw new ArgumentNullException(nameof(databaseConnectionString));
+            }
+
+            DatabaseConnectionString = databaseConnectionString;
+        }
+
+        public bool IsFirstRun()
+        {
+            using (var conn = new SqlConnection(DatabaseConnectionString))
+            {
+                var tableExists = conn.ExecuteScalar<int>("SELECT TOP 1 1 " +
+                                                          "FROM INFORMATION_SCHEMA.TABLES " +
+                                                          "WHERE TABLE_NAME = N'Link'") == 1;
+                return !tableExists;
+            }
+        }
+
+        public bool TestDatabaseConnection(Action<Exception> errorLogAction = null)
+        {
+            try
+            {
+                using (var conn = new SqlConnection(DatabaseConnectionString))
+                {
+                    int result = conn.ExecuteScalar<int>("SELECT 1");
+                    return result == 1;
+                }
+            }
+            catch (Exception e)
+            {
+                errorLogAction?.Invoke(e);
+                return false;
+            }
+        }
+
+        public Response SetupDatabase()
+        {
+            try
+            {
+                using (var conn = new SqlConnection(DatabaseConnectionString))
+                {
+                    var sql = GetEmbeddedSqlScript("schema-mssql-140");
+                    if (!string.IsNullOrWhiteSpace(sql))
+                    {
+                        conn.Execute(sql);
+                        return new SuccessResponse();
+                    }
+                    return new FailedResponse("Database Schema Script is empty.");
+                }
+            }
+            catch (Exception e)
+            {
+                return new FailedResponse(e.Message);
+            }
+        }
+
+        private static string GetEmbeddedSqlScript(string scriptName)
+        {
+            var assembly = typeof(SetupHelper).GetTypeInfo().Assembly;
+            using (var stream = assembly.GetManifestResourceStream($"LinkForwarder.Data.{scriptName}.sql"))
+            {
+                if (stream == null) return null;
+                using (var reader = new StreamReader(stream))
+                {
+                    var sql = reader.ReadToEnd();
+                    return sql;
+                }
+            }
+        }
+    }
+}
