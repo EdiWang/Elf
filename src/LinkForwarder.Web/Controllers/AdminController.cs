c672a692fade25f5add88fa1374dedcf936f73a0
Edi Wang <Edi.Wang@outlook.com>
2019-07-08 12:51:14 +0800

    merge controllers to simplify project

--- src/LinkForwarder.Web/Controllers/AdminController.cs
+++ src/LinkForwarder.Web/Controllers/AdminController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using LinkForwarder.Services;
+using LinkForwarder.Services.Entities;
+using LinkForwarder.Services.Models;
 using LinkForwarder.Web.Authentication;
 using LinkForwarder.Web.Models;
 using Microsoft.AspNetCore.Authentication;
@@ -10,6 +13,7 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 
 namespace LinkForwarder.Web.Controllers
@@ -20,12 +24,20 @@ namespace LinkForwarder.Web.Controllers
     {
         private readonly AuthenticationSettings _authenticationSettings;
         private readonly ILogger<AdminController> _logger;
+        private readonly ILinkForwarderService _linkForwarderService;
+        private readonly ILinkVerifier _linkVerifier;
+        private readonly IMemoryCache _cache;
 
         public AdminController(
-            ILogger<AdminController> logger)
+            ILogger<AdminController> logger,
+            ILinkForwarderService linkForwarderService,
+            ILinkVerifier linkVerifier,
+            IMemoryCache cache)
         {
             _logger = logger;
-
+            _linkForwarderService = linkForwarderService;
+            _linkVerifier = linkVerifier;
+            _cache = cache;
             _authenticationSettings = AppDomain.CurrentDomain.GetData(nameof(AuthenticationSettings)) as AuthenticationSettings;
         }
 
@@ -39,7 +51,7 @@ namespace LinkForwarder.Web.Controllers
             {
                 case AuthenticationProvider.AzureAD:
                     {
-                        var redirectUrl = Url.Action("Manage", "Link");
+                        var redirectUrl = Url.Action("Index", "Admin");
                         return Challenge(
                             new AuthenticationProperties { RedirectUri = redirectUrl },
                             OpenIdConnectDefaults.AuthenticationScheme);
@@ -130,8 +142,7 @@ namespace LinkForwarder.Web.Controllers
         [HttpGet("accessdenied")]
         public IActionResult AccessDenied()
         {
-            HttpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
-            return View();
+            return StatusCode(StatusCodes.Status403Forbidden);
         }
 
         #endregion
@@ -139,7 +150,215 @@ namespace LinkForwarder.Web.Controllers
         [Route("")]
         public IActionResult Index()
         {
-            return RedirectToAction("Manage", "Link");
+            return View();
+        }
+
+
+        [Route("report")]
+        public IActionResult Report()
+        {
+            return View();
+        }
+
+        [Route("tracking-count-past-week")]
+        public async Task<IActionResult> TrackingCountPastWeek()
+        {
+            var response = await _linkForwarderService.GetLinkTrackingDateCount(7);
+            if (!response.IsSuccess)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+            }
+
+            return Json(response);
+        }
+
+        [Route("client-type-past-month")]
+        public async Task<IActionResult> ClientTypePastMonth()
+        {
+            var response = await _linkForwarderService.GetClientTypeCounts(30);
+            if (!response.IsSuccess)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+            }
+
+            return Json(response);
+        }
+
+        [Route("most-requested-links-past-month")]
+        public async Task<IActionResult> MostRequestedLinksPastMonth()
+        {
+            var response = await _linkForwarderService.GetMostRequestedLinkCount(30);
+            if (!response.IsSuccess)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+            }
+
+            return Json(response);
+        }
+
+        [Route("recent-requests")]
+        public async Task<IActionResult> RecentRequests()
+        {
+            var response = await _linkForwarderService.GetRecentRequests(64);
+            if (!response.IsSuccess)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+            }
+
+            return Json(new { Data = response.Item });
+        }
+
+        [HttpPost]
+        [Route("list")]
+        public async Task<IActionResult> List(DataTableRequest model)
+        {
+            var searchBy = model.Search?.Value;
+            var take = model.Length;
+            var offset = model.Start;
+
+            var response = await _linkForwarderService.GetPagedLinksAsync(offset, take, searchBy);
+            if (response.IsSuccess)
+            {
+                var jqdtResponse = new JqDataTableResponse<Link>
+                {
+                    Draw = model.Draw,
+                    RecordsFiltered = response.Item.TotalRows,
+                    RecordsTotal = response.Item.TotalRows,
+                    Data = response.Item.Links
+                };
+                return Json(jqdtResponse);
+            }
+            return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+        }
+
+        [HttpPost]
+        [Route("create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(LinkEditModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var verifyResult = _linkVerifier.Verify(model.OriginUrl, Url, Request);
+                switch (verifyResult)
+                {
+                    case LinkVerifyResult.InvalidFormat:
+                        return BadRequest("Not a valid URL.");
+                    case LinkVerifyResult.InvalidLocal:
+                        return BadRequest("Can not use local URL.");
+                    case LinkVerifyResult.InvalidSelfReference:
+                        return BadRequest("Can not use url pointing to this site.");
+                }
+
+                var createLinkRequest = new CreateLinkRequest
+                {
+                    OriginUrl = model.OriginUrl,
+                    Note = model.Note,
+                    AkaName = string.IsNullOrWhiteSpace(model.AkaName) ? null : model.AkaName,
+                    IsEnabled = model.IsEnabled
+                };
+
+                var response = await _linkForwarderService.CreateLinkAsync(createLinkRequest);
+                return Json(response);
+            }
+            return BadRequest("Invalid ModelState");
+        }
+
+        [Route("get-edit-model/{id}")]
+        public async Task<IActionResult> GetEditModel(int id)
+        {
+            var linkResponse = await _linkForwarderService.GetLinkAsync(id);
+            if (!linkResponse.IsSuccess)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (null == linkResponse.Item)
+            {
+                return NotFound();
+            }
+
+            var model = new LinkEditModel
+            {
+                Id = linkResponse.Item.Id,
+                Note = linkResponse.Item.Note,
+                AkaName = linkResponse.Item.AkaName,
+                OriginUrl = linkResponse.Item.OriginUrl,
+                IsEnabled = linkResponse.Item.IsEnabled
+            };
+
+            return Json(model);
+        }
+
+        [HttpPost("edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(LinkEditModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var verifyResult = _linkVerifier.Verify(model.OriginUrl, Url, Request);
+                switch (verifyResult)
+                {
+                    case LinkVerifyResult.InvalidFormat:
+                        return BadRequest("Not a valid URL.");
+                    case LinkVerifyResult.InvalidLocal:
+                        return BadRequest("Can not use local URL.");
+                    case LinkVerifyResult.InvalidSelfReference:
+                        return BadRequest("Can not use url pointing to this site.");
+                }
+
+                var editRequest = new EditLinkRequest(model.Id)
+                {
+                    NewUrl = model.OriginUrl,
+                    Note = model.Note,
+                    AkaName = string.IsNullOrWhiteSpace(model.AkaName) ? null : model.AkaName,
+                    IsEnabled = model.IsEnabled
+                };
+
+                var response = await _linkForwarderService.EditLinkAsync(editRequest);
+                if (response.IsSuccess)
+                {
+                    _cache.Remove(response.Item);
+                }
+                return Json(response);
+            }
+            return BadRequest("Invalid ModelState");
+        }
+
+        [Route("delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteLink(int linkId)
+        {
+            var linkResponse = await _linkForwarderService.GetLinkAsync(linkId);
+            if (!linkResponse.IsSuccess)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (null == linkResponse.Item)
+            {
+                return BadRequest();
+            }
+
+            var response = await _linkForwarderService.DeleteLink(linkId);
+
+            if (response.IsSuccess)
+            {
+                _cache.Remove(linkResponse.Item);
+                return Content(linkId.ToString());
+            }
+            return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+        }
+
+        [Route("clear-tracking-data")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ClearTrackingData(int nonce)
+        {
+            var response = await _linkForwarderService.ClearTrackingDataAsync();
+            if (!response.IsSuccess)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+            }
+            return Json(response);
         }
     }
 }
