00a4e242e305785a6482c3ef412703763029bb6a
Edi Wang <Edi.Wang@outlook.com>
2019-09-21 10:02:10 +0800

    Add UT

---
+++ src/LinkForwarder.Tests/TokenGeneratorTests.cs
@@ -0,0 +1,36 @@
+using LinkForwarder.Services.TokenGenerator;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Tests
+{
+    public class TokenGeneratorTests
+    {
+        public ITokenGenerator TGen { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            TGen = new ShortGuidTokenGenerator();
+        }
+
+        [Test]
+        public void TestTokenFormat()
+        {
+            string token = TGen.GenerateToken();
+            var hasUpperCase = token.Any(char.IsUpper);
+            Assert.IsTrue(token.Length == 8);
+            Assert.IsTrue(!hasUpperCase);
+        }
+
+        [TestCase("996", ExpectedResult = false)]
+        [TestCase("", ExpectedResult = false)]
+        [TestCase("Work 996 and get into ICU", ExpectedResult = false)]
+        [TestCase("love.net", ExpectedResult = true)]
+        public bool TestTokenParser(string token)
+        {
+            var b = TGen.TryParseToken(token, out var t);
+            return b;
+        }
+    }
+}
