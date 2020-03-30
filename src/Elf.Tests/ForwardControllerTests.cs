de7dac595dbf20057366d1bbded8a7a740bc796e
Edi Wang <Edi.Wang@outlook.com>
2020-03-30 12:59:16 +0800

    Refact UT

--- src/Elf.Tests/ForwardControllerTests.cs
+++ src/Elf.Tests/ForwardControllerTests.cs
@@ -33,6 +33,10 @@ namespace Elf.Tests
         {
             _loggerMock = new Mock<ILogger<ForwardController>>();
             _appSettingsMock = new Mock<IOptions<AppSettings>>();
+            _tokenGeneratorMock = new Mock<ITokenGenerator>();
+            _linkVerifierMock = new Mock<ILinkVerifier>();
+            _linkForwarderServiceMock = new Mock<ILinkForwarderService>();
+            _memoryCacheMock = new Mock<IMemoryCache>();
         }
 
         [TestCase("")]
@@ -40,11 +44,6 @@ namespace Elf.Tests
         [TestCase(null)]
         public async Task TestForwardEmptyToken(string token)
         {
-            _tokenGeneratorMock = new Mock<ITokenGenerator>();
-            _linkVerifierMock = new Mock<ILinkVerifier>();
-            _linkForwarderServiceMock = new Mock<ILinkForwarderService>();
-            _memoryCacheMock = new Mock<IMemoryCache>();
-
             var ctl = new ForwardController(
                 _appSettingsMock.Object,
                 _loggerMock.Object,
@@ -62,11 +61,6 @@ namespace Elf.Tests
         [TestCase(null)]
         public async Task TestForwardEmptyUA(string ua)
         {
-            _tokenGeneratorMock = new Mock<ITokenGenerator>();
-            _linkVerifierMock = new Mock<ILinkVerifier>();
-            _linkForwarderServiceMock = new Mock<ILinkForwarderService>();
-            _memoryCacheMock = new Mock<IMemoryCache>();
-
             var ctl = new ForwardController(
                 _appSettingsMock.Object,
                 _loggerMock.Object,
@@ -92,13 +86,8 @@ namespace Elf.Tests
         {
             string inputToken = "996";
             string t;
-            _tokenGeneratorMock = new Mock<ITokenGenerator>();
-            _tokenGeneratorMock.Setup(tg => tg.TryParseToken(inputToken, out t))
+            _tokenGeneratorMock.Setup(p => p.TryParseToken(inputToken, out t))
                                .Returns(false);
-
-            _linkVerifierMock = new Mock<ILinkVerifier>();
-            _linkForwarderServiceMock = new Mock<ILinkForwarderService>();
-            _memoryCacheMock = new Mock<IMemoryCache>();
 
             var ctl = new ForwardController(
                 _appSettingsMock.Object,
@@ -125,21 +114,16 @@ namespace Elf.Tests
         {
             string inputToken = "996";
             string t;
-            _tokenGeneratorMock = new Mock<ITokenGenerator>();
-            _tokenGeneratorMock.Setup(tg => tg.TryParseToken(inputToken, out t))
-                .Returns(true);
+            _tokenGeneratorMock.Setup(p => p.TryParseToken(inputToken, out t))
+                               .Returns(true);
 
-            _linkVerifierMock = new Mock<ILinkVerifier>();
-            _linkForwarderServiceMock = new Mock<ILinkForwarderService>();
-            _memoryCacheMock = new Mock<IMemoryCache>();
-
-            var link = new Link()
+            var link = new Link
             {
                 OriginUrl = "https://996.icu"
             };
 
             var memoryCache = MockMemoryCacheService.GetMemoryCache(link);
-            var cachedResponse = memoryCache.Get<Link>(inputToken);
+            // var cachedResponse = memoryCache.Get<Link>(inputToken);
 
             _appSettingsMock.Setup(p => p.Value).Returns(new AppSettings()
             {
