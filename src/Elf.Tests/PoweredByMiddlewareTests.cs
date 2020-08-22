93dcebe0c240c2f921c3e9485a3359db8ad1b71a
Edi Wang <Edi.Wang@outlook.com>
2020-08-22 18:25:32 +0800

    Create PoweredByMiddlewareTests.cs

---
+++ src/Elf.Tests/PoweredByMiddlewareTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Elf.Web.Middleware;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using Moq;
+using NUnit.Framework;
+
+namespace Elf.Tests
+{
+    [TestFixture]
+    public class PoweredByMiddlewareTests
+    {
+        [Test]
+        public async Task TestPoweredByHeader()
+        {
+            const string key = "X-Powered-By";
+            const string value = "Elf";
+
+            var headersArray = new Dictionary<string, StringValues> { { key, string.Empty } };
+            var headersDic = new HeaderDictionary(headersArray);
+            var httpResponseMock = new Mock<HttpResponse>();
+            httpResponseMock.SetupGet(r => r.Headers).Returns(headersDic);
+
+            var httpContextMock = new Mock<HttpContext>();
+            httpContextMock.Setup(c => c.Response).Returns(httpResponseMock.Object);
+
+            static Task RequestDelegate(HttpContext context) => Task.CompletedTask;
+            var middleware = new PoweredByMiddleware(RequestDelegate);
+
+            await middleware.Invoke(httpContextMock.Object);
+
+            Assert.AreEqual(value, headersArray[key]);
+            httpResponseMock.Verify(r => r.Headers, Times.Once);
+        }
+    }
+}
