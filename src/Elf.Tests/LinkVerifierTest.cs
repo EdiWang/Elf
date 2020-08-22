e3431bc4655ee9fcb8ad18a2349b83fe17f14e66
Edi Wang <Edi.Wang@outlook.com>
2020-08-22 17:43:17 +0800

    Update LinkVerifierTest.cs

--- src/Elf.Tests/LinkVerifierTest.cs
+++ src/Elf.Tests/LinkVerifierTest.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 using Elf.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using NUnit.Framework;
 
 namespace Elf.Tests
@@ -9,7 +12,7 @@ namespace Elf.Tests
     [TestFixture]
     public class LinkVerifierTest
     {
-        private LinkVerifier _linkVerifier = new LinkVerifier();
+        private readonly LinkVerifier _linkVerifier = new LinkVerifier();
 
         [TestCase("https://go.edi.wang", ExpectedResult = false)]
         [TestCase("https://go.edi.wang/", ExpectedResult = false)]
@@ -30,6 +33,38 @@ namespace Elf.Tests
         public bool TestIsForwardEndpoint(string url)
         {
             return _linkVerifier.IsForwardEndpoint(new Uri(url));
+        }
+
+        [Test]
+        public void TestVerifyInvalidFormat()
+        {
+            var urlHelperMock = new Mock<IUrlHelper>();
+            var result = _linkVerifier.Verify("996", urlHelperMock.Object, null);
+            Assert.AreEqual(LinkVerifyResult.InvalidFormat, result);
+        }
+
+        [Test]
+        public void TestVerifyInvalidLocal()
+        {
+            var urlHelperMock = new Mock<IUrlHelper>();
+            urlHelperMock.Setup(p => p.IsLocalUrl(It.IsAny<string>())).Returns(true);
+
+            var result = _linkVerifier.Verify("https://localhost/996", urlHelperMock.Object, null);
+            Assert.AreEqual(LinkVerifyResult.InvalidLocal, result);
+        }
+
+        [Test]
+        public void TestSelfReferenceNoForwardEndpoint()
+        {
+            var urlHelperMock = new Mock<IUrlHelper>();
+            urlHelperMock.Setup(p => p.IsLocalUrl(It.IsAny<string>())).Returns(false);
+
+            var reqMock = new Mock<HttpRequest>();
+            reqMock.SetupGet(r => r.Host).Returns(new HostString("edi.wang"));
+            reqMock.SetupGet(r => r.Scheme).Returns("https");
+
+            var result = _linkVerifier.Verify("https://edi.wang/1055", urlHelperMock.Object, reqMock.Object);
+            Assert.AreEqual(LinkVerifyResult.Valid, result);
         }
     }
 }
