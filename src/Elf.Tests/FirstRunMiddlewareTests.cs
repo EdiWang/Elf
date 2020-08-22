73934f7fd9f30996f9ac588d1814eada7f549afe
Edi Wang <Edi.Wang@outlook.com>
2020-08-22 18:25:36 +0800

    Create FirstRunMiddlewareTests.cs

---
+++ src/Elf.Tests/FirstRunMiddlewareTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Elf.Web.Middleware;
+using Microsoft.AspNetCore.Http;
+using NUnit.Framework;
+
+namespace Elf.Tests
+{
+    [TestFixture]
+    public class FirstRunMiddlewareTests
+    {
+        [Test]
+        public async Task TestFirstRunWithToken()
+        {
+            var ctx = new DefaultHttpContext();
+            AppDomain.CurrentDomain.SetData("FIRSTRUN_INIT_SUCCESS", true);
+
+            static Task RequestDelegate(HttpContext context) => Task.CompletedTask;
+            var middleware = new FirstRunMiddleware(RequestDelegate);
+            await middleware.Invoke(ctx, null, null);
+
+            Assert.Pass();
+        }
+    }
+}
