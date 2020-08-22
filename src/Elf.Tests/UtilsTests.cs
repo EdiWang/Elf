3e912a5c028a94282211059e95a41da3e034af79
Edi Wang <Edi.Wang@outlook.com>
2020-08-22 18:20:48 +0800

    Create UtilsTests.cs

---
+++ src/Elf.Tests/UtilsTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Elf.Web;
+using NUnit.Framework;
+
+namespace Elf.Tests
+{
+    [TestFixture]
+    public class UtilsTests
+    {
+        [Test]
+        public void TestAppVersion()
+        {
+            Assert.IsNotNull(Utils.AppVersion);
+        }
+
+        [Test]
+        public void TestIsValidUrlUnknownSchema()
+        {
+            Assert.Throws(typeof(ArgumentOutOfRangeException), () =>
+            {
+                "https://996.icu".IsValidUrl((Utils.UrlScheme)4);
+            });
+        }
+
+        [TestCase("https://996.icu", ExpectedResult = true)]
+        [TestCase("http://996.rip", ExpectedResult = false)]
+        public bool TestIsValidUrlHttps(string str)
+        {
+            return str.IsValidUrl(Utils.UrlScheme.Https);
+        }
+
+        [TestCase("https://996.icu", ExpectedResult = false)]
+        [TestCase("http://996.rip", ExpectedResult = true)]
+        public bool TestIsValidUrlHttp(string str)
+        {
+            return str.IsValidUrl(Utils.UrlScheme.Http);
+        }
+    }
+}
