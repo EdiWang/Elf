474cb2d677b1ea6c780db03b190f93b1571c4c24
Edi Wang <Edi.Wang@outlook.com>
2020-11-15 14:12:53 +0800

    SetupHelperTests

---
+++ src/Elf.Tests/SetupHelperTests.cs
@@ -0,0 +1,47 @@
+using Elf.Setup;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using Dapper;
+using Moq.Dapper;
+
+namespace Elf.Tests
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class SetupHelperTests
+    {
+        private MockRepository mockRepository;
+
+        private Mock<IDbConnection> mockDbConnection;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mockRepository = new MockRepository(MockBehavior.Default);
+            mockDbConnection = mockRepository.Create<IDbConnection>();
+        }
+
+        [Test]
+        public void IsFirstRun_Yes()
+        {
+            mockDbConnection.SetupDapper(c => c.ExecuteScalar<int>(It.IsAny<string>(), null, null, null, null)).Returns(0);
+            var setupHelper = new SetupHelper(mockDbConnection.Object);
+
+            var result = setupHelper.IsFirstRun();
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void IsFirstRun_No()
+        {
+            mockDbConnection.SetupDapper(c => c.ExecuteScalar<int>(It.IsAny<string>(), null, null, null, null)).Returns(1);
+            var setupHelper = new SetupHelper(mockDbConnection.Object);
+
+            var result = setupHelper.IsFirstRun();
+            Assert.IsFalse(result);
+        }
+    }
+}
