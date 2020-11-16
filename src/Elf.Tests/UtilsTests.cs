8e74733a3d68afb2506ff0b5be48fd8b13713595
Edi Wang <Edi.Wang@outlook.com>
2020-11-16 10:07:27 +0800

    Update UtilsTests.cs

--- src/Elf.Tests/UtilsTests.cs
+++ src/Elf.Tests/UtilsTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Elf.Web;
 using NUnit.Framework;
 
@@ -36,6 +37,59 @@ namespace Elf.Tests
         public bool IsValidUrl_Http(string str)
         {
             return str.IsValidUrl(Utils.UrlScheme.Http);
+        }
+
+        [TestCase("")]
+        [TestCase("DC1")]
+        [TestCase("DC1,DC2")]
+        [TestCase("DC1, DC2")]
+        [TestCase("DC1, DC2,DC3")]
+        [TestCase("DC[1], DC-2, DC#3, DC@4, DC$5, DC(6), DC/7")]
+        public void GetEnvironmentTags_Valid(string tags)
+        {
+            Environment.SetEnvironmentVariable("ELF_TAGS", tags, EnvironmentVariableTarget.Process);
+            var envTags = Utils.GetEnvironmentTags();
+            Assert.IsNotNull(envTags);
+
+            var list = tags.Split(',').Select(p => p.Trim());
+            foreach (var tag in list)
+            {
+                Assert.IsTrue(envTags.Contains(tag));
+            }
+        }
+
+        [TestCase("DC%1")]
+        [TestCase("DC 1")]
+        [TestCase("DC*1")]
+        [TestCase("DC^1")]
+        [TestCase("DC^1")]
+        [TestCase("DC+1")]
+        [TestCase("DC=1")]
+        [TestCase("DC!1")]
+        [TestCase("DC`1")]
+        [TestCase("DC~1")]
+        [TestCase("DC'1")]
+        [TestCase("DC?1")]
+        [TestCase("DC{1}")]
+        public void GetEnvironmentTags_Invalid(string invalidTag)
+        {
+            Environment.SetEnvironmentVariable("ELF_TAGS", $"DC1, DC2, {invalidTag}", EnvironmentVariableTarget.Process);
+            var envTags = Utils.GetEnvironmentTags();
+            Assert.IsNotNull(envTags);
+
+            Assert.IsTrue(envTags.Count() == 2);
+            Assert.IsTrue(!envTags.Contains(invalidTag));
+        }
+
+        [Test]
+        public void GetEnvironmentTags_Empty()
+        {
+            Environment.SetEnvironmentVariable("ELF_TAGS", string.Empty, EnvironmentVariableTarget.Process);
+            var envTags = Utils.GetEnvironmentTags();
+            Assert.IsNotNull(envTags);
+
+            Assert.IsTrue(envTags.Count() == 1);
+            Assert.AreEqual(envTags.First(), string.Empty);
         }
     }
 }
