8c575c309c9d6490b1061269d12abac9046cadd4
Edi Wang <Edi.Wang@outlook.com>
2020-11-28 18:32:32 +0800

    update entity attributes

--- src/Elf.Services/Entities/Link.cs
+++ src/Elf.Services/Entities/Link.cs
@@ -3,23 +3,31 @@ using LinqToDB.Mapping;
 
 namespace Elf.Services.Entities
 {
+    [Table]
     public class Link
     {
         [PrimaryKey, Identity]
         public int Id { get; set; }
 
+        [Column]
         public string OriginUrl { get; set; }
 
+        [Column]
         public string FwToken { get; set; }
 
+        [Column]
         public string Note { get; set; }
 
+        [Column]
         public string AkaName { get; set; }
 
+        [Column]
         public bool IsEnabled { get; set; }
 
+        [Column]
         public DateTime UpdateTimeUtc { get; set; }
 
+        [Column]
         public int? TTL { get; set; }
     }
 }
--- src/Elf.Services/Entities/LinkTracking.cs
+++ src/Elf.Services/Entities/LinkTracking.cs
@@ -3,17 +3,22 @@ using LinqToDB.Mapping;
 
 namespace Elf.Services.Entities
 {
+    [Table]
     public class LinkTracking
     {
         [PrimaryKey]
         public Guid Id { get; set; } = Guid.NewGuid();
 
+        [Column]
         public int LinkId { get; set; }
 
+        [Column]
         public string UserAgent { get; set; }
 
+        [Column]
         public string IpAddress { get; set; }
 
+        [Column]
         public DateTime RequestTimeUtc { get; set; }
     }
 }
