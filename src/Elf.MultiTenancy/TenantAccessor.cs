056fbf7f70ebc6084f71f7058b11a7ab90cff101
Edi Wang <Edi.Wang@outlook.com>
2021-03-16 12:47:54 +0800

    Create TenantAccessor.cs

---
+++ src/Elf.MultiTenancy/TenantAccessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Elf.MultiTenancy
+{
+    public interface ITenantAccessor<T> where T : Tenant
+    {
+        T Tenant { get; }
+    }
+
+    public class TenantAccessor<T> : ITenantAccessor<T> where T : Tenant
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public TenantAccessor(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public T Tenant => _httpContextAccessor.HttpContext.GetTenant<T>();
+    }
+}
