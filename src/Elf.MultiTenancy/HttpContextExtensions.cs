1fc3a8c4637a1368f9b82e7e4e446b6b8b4e8794
Edi Wang <Edi.Wang@outlook.com>
2021-03-16 12:45:15 +0800

    Create HttpContextExtensions.cs

---
+++ src/Elf.MultiTenancy/HttpContextExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Elf.MultiTenancy
+{
+    /// <summary>
+    /// Extensions to HttpContext to make multi-tenancy easier to use
+    /// </summary>
+    public static class HttpContextExtensions
+    {
+        /// <summary>
+        /// Returns the current tenant
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static T GetTenant<T>(this HttpContext context) where T : Tenant
+        {
+            if (!context.Items.ContainsKey(Constants.HttpContextTenantKey))
+                return null;
+            return context.Items[Constants.HttpContextTenantKey] as T;
+        }
+
+        /// <summary>
+        /// Returns the current Tenant
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static Tenant GetTenant(this HttpContext context)
+        {
+            return context.GetTenant<Tenant>();
+        }
+    }
+}
