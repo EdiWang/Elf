50c6aee300367c36665475024ba7582851a65d98
Edi Wang <Edi.Wang@outlook.com>
2021-03-16 12:27:49 +0800

    Create Tenant.cs

---
+++ src/Elf.MultiTenancy/Tenant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elf.MultiTenancy
+{
+    /// <summary>
+    /// Tenant information
+    /// </summary>
+    public class Tenant
+    {
+        /// <summary>
+        /// The tenant Id
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The tenant identifier
+        /// </summary>
+        public string Identifier { get; set; }
+
+        /// <summary>
+        /// Tenant items
+        /// </summary>
+        public Dictionary<string, object> Items { get; } = new();
+    }
+}
