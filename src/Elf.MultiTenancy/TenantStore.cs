cb10489505951e6d9b1cac61dd390076269ff93d
Edi Wang <Edi.Wang@outlook.com>
2021-03-16 13:14:13 +0800

    Update TenantStore.cs

--- src/Elf.MultiTenancy/TenantStore.cs
+++ src/Elf.MultiTenancy/TenantStore.cs
@@ -25,7 +25,14 @@ namespace Elf.MultiTenancy
         {
             var tenant = new[]
             {
-                new Tenant{ Id = "80fdb3c0-5888-4295-bf40-ebee0e3cd8f3", Identifier = "localhost" }
+                new Tenant{
+                    Id = "80fdb3c0-5888-4295-bf40-ebee0e3cd8f3",
+                    Identifier = "localhost",
+                    Items = new()
+                    {
+                        { "DefaultRedirectionUrl","https://greenhat.today" }
+                    }
+                }
             }.SingleOrDefault(t => t.Identifier == identifier);
 
             return await Task.FromResult(tenant);
