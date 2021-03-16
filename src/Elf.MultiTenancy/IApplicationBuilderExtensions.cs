99c959b6fadceaf6cf7012cd7aa4eb6204374fe2
Edi Wang <Edi.Wang@outlook.com>
2021-03-16 12:43:08 +0800

    Create IApplicationBuilderExtensions.cs

---
+++ src/Elf.MultiTenancy/IApplicationBuilderExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace Elf.MultiTenancy
+{
+    /// <summary>
+    /// Nice method to register our middleware
+    /// </summary>
+    public static class IApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Use the Teanant Middleware to process the request
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseMultiTenancy<T>(this IApplicationBuilder builder) where T : Tenant
+            => builder.UseMiddleware<TenantMiddleware<T>>();
+
+
+        /// <summary>
+        /// Use the Teanant Middleware to process the request
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseMultiTenancy(this IApplicationBuilder builder)
+            => builder.UseMiddleware<TenantMiddleware<Tenant>>();
+    }
+}
