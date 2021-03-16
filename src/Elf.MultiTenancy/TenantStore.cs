6a1bef6a3e9df05a41ab35817335cf94c4fa7b07
Edi Wang <Edi.Wang@outlook.com>
2021-03-16 12:29:31 +0800

    Create TenantStore.cs

---
+++ src/Elf.MultiTenancy/TenantStore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elf.MultiTenancy
+{
+    public interface ITenantStore<T> where T : Tenant
+    {
+        Task<T> GetTenantAsync(string identifier);
+    }
+
+    /// <summary>
+    /// In memory store for testing
+    /// </summary>
+    public class InMemoryTenantStore : ITenantStore<Tenant>
+    {
+        /// <summary>
+        /// Get a tenant for a given identifier
+        /// </summary>
+        /// <param name="identifier"></param>
+        /// <returns></returns>
+        public async Task<Tenant> GetTenantAsync(string identifier)
+        {
+            var tenant = new[]
+            {
+                new Tenant{ Id = "80fdb3c0-5888-4295-bf40-ebee0e3cd8f3", Identifier = "localhost" }
+            }.SingleOrDefault(t => t.Identifier == identifier);
+
+            return await Task.FromResult(tenant);
+        }
+    }
+}
