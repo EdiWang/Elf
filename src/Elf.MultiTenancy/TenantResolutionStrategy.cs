fe8572d1ce52e6144856a6a5ce7b6b88e9340588
Edi Wang <Edi.Wang@outlook.com>
2021-03-16 12:27:53 +0800

    Create TenantResolutionStrategy.cs

---
+++ src/Elf.MultiTenancy/TenantResolutionStrategy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Elf.MultiTenancy
+{
+    public interface ITenantResolutionStrategy
+    {
+        Task<string> GetTenantIdentifierAsync();
+    }
+
+    /// <summary>
+    /// Resolve the host to a tenant identifier
+    /// </summary>
+    public class HostResolutionStrategy : ITenantResolutionStrategy
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public HostResolutionStrategy(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        /// <summary>
+        /// Get the tenant identifier
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task<string> GetTenantIdentifierAsync()
+        {
+            return await Task.FromResult(_httpContextAccessor.HttpContext.Request.Host.Host);
+        }
+    }
+}
