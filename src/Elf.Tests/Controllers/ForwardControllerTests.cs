becea35ac3717c481d9be8dda1853ccedee27af9
Edi Wang <Edi.Wang@outlook.com>
2021-05-29 19:13:48 +0800

    code clean up

--- src/Elf.Tests/Controllers/ForwardControllerTests.cs
+++ src/Elf.Tests/Controllers/ForwardControllerTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Elf.MultiTenancy;
--- src/Elf.Web/Startup.cs
+++ src/Elf.Web/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Text;
 using AspNetCoreRateLimit;
 using Elf.MultiTenancy;
 using Elf.Services;
