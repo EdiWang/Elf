c64c94cdc38be58f822df9e99a8bf7cf6be9608c
Edi Wang <Edi.Wang@outlook.com>
2021-06-03 12:43:52 +0800

    code refact

--- src/Elf.Web/wwwroot/js/site.js
+++ src/Elf.Web/wwwroot/js/site.js
@@ -1,19 +1,4 @@
 ﻿var csrfFieldName = 'CSRF-TOKEN-ELF-FORM';
-function ajaxPostWithCSRFToken(url, pData, funcSuccess) {
-    var options = {
-        type: 'POST',
-        url: url,
-        headers: {},
-        data: pData,
-        success: function (data) {
-            funcSuccess(data);
-        },
-        dataType: 'json'
-    };
-    options.headers['XSRF-TOKEN'] = $(`input[name=${csrfFieldName}]`).val();
-    $.ajax(options);
-}
-
 function callApi(uri, method, request, funcSuccess, funcAlways) {
     const csrfValue = $(`input[name=${csrfFieldName}]`).val();
     fetch(uri, {
