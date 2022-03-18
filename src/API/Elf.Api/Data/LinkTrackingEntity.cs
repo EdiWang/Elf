8b40692efb812a4603bc63a9cb10823946e2518d
Edi Wang <Edi.Wang@outlook.com>
2022-03-18 13:45:25 +0800

    Update LinkTrackingEntity.cs

--- src/API/Elf.Api/Data/LinkTrackingEntity.cs
+++ src/API/Elf.Api/Data/LinkTrackingEntity.cs
@@ -5,17 +5,27 @@ namespace Elf.Api.Data;
 
 public class LinkTrackingEntity
 {
-    public Guid Id { get; set; } = Guid.NewGuid(); // uniqueidentifier
+    public Guid Id { get; set; } = Guid.NewGuid();
 
-    public Guid TenantId { get; set; } // uniqueidentifier
+    public Guid TenantId { get; set; }
 
-    public int LinkId { get; set; } // int
+    public int LinkId { get; set; }
 
-    public string UserAgent { get; set; } // nvarchar(256)
+    public string UserAgent { get; set; } 
 
-    public string IpAddress { get; set; } // varchar(64)
+    public string IpAddress { get; set; }
 
-    public DateTime RequestTimeUtc { get; set; } // datetime
+    public string IPCountry { get; set; }
+
+    public string IPRegion { get; set; }
+
+    public string IPCity { get; set; }
+
+    public string IPASN { get; set; }
+
+    public string IPOrg { get; set; }
+
+    public DateTime RequestTimeUtc { get; set; }
 
     public virtual LinkEntity Link { get; set; }
 }
@@ -26,6 +36,11 @@ internal class LinkTrackingEntityConfiguration : IEntityTypeConfiguration<LinkTr
     {
         builder.Property(e => e.UserAgent).HasMaxLength(256);
         builder.Property(e => e.IpAddress).HasMaxLength(64);
+        builder.Property(e => e.IPCountry).HasMaxLength(64);
+        builder.Property(e => e.IPRegion).HasMaxLength(64);
+        builder.Property(e => e.IPCity).HasMaxLength(64);
+        builder.Property(e => e.IPASN).HasMaxLength(16);
+        builder.Property(e => e.IPOrg).HasMaxLength(64);
         builder.Property(e => e.RequestTimeUtc).HasColumnType("datetime");
 
         builder.HasOne(d => d.Link)
