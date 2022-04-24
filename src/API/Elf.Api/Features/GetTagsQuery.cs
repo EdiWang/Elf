38510112b6729f7c857182ab315752c57eec909e
Edi Wang <Edi.Wang@outlook.com>
2022-04-24 19:04:04 +0800

    Create GetTagsQuery.cs

---
+++ src/API/Elf.Api/Features/GetTagsQuery.cs
@@ -0,0 +1,15 @@
+﻿using Elf.Api.Data;
+
+namespace Elf.Api.Features;
+
+public record GetTagsQuery : IRequest<List<TagEntity>>;
+
+public class GetTagsQueryHandler : IRequestHandler<GetTagsQuery, List<TagEntity>>
+{
+
+
+    public Task<List<TagEntity>> Handle(GetTagsQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
