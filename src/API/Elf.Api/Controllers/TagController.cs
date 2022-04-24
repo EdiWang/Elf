ecdcc0af248f7981ffeae11b1d763fac4c5fd44d
Edi Wang <Edi.Wang@outlook.com>
2022-04-24 16:55:04 +0800

    Update TagController.cs

--- src/API/Elf.Api/Controllers/TagController.cs
+++ src/API/Elf.Api/Controllers/TagController.cs
@@ -1,11 +1,23 @@
-﻿namespace Elf.Api.Controllers;
+﻿using System.ComponentModel.DataAnnotations;
+using Elf.Api.Features;
 
+namespace Elf.Api.Controllers;
+
+[Authorize]
 [ApiController]
+[Route("api/[controller]")]
 public class TagController : ControllerBase
 {
     private readonly IMediator _mediator;
 
     public TagController(IMediator mediator) => _mediator = mediator;
 
-    
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Create([FromBody][Required][MaxLength(32)] string name)
+    {
+        await _mediator.Send(new CreateTagCommand(name));
+        return NoContent();
+    }
 }
