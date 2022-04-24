a8d9ad83238188902d52d1f934245561e5a28063
Edi Wang <Edi.Wang@outlook.com>
2022-04-24 21:01:54 +0800

    Update DeleteTagCommand.cs

--- src/API/Elf.Api/Features/DeleteTagCommand.cs
+++ src/API/Elf.Api/Features/DeleteTagCommand.cs
@@ -1,3 +1,22 @@
-﻿namespace Elf.Api.Features;
+﻿using Elf.Api.Data;
+
+namespace Elf.Api.Features;
 
 public record DeleteTagCommand(int Id) : IRequest<int>;
+
+public class DeleteTagCommandHandler : IRequestHandler<DeleteTagCommand, int>
+{
+    private readonly ElfDbContext _dbContext;
+
+    public DeleteTagCommandHandler(ElfDbContext dbContext) => _dbContext = dbContext;
+
+    public async Task<int> Handle(DeleteTagCommand request, CancellationToken cancellationToken)
+    {
+        var tag = await _dbContext.Tag.FindAsync(request.Id);
+        if (null == tag) return -1;
+
+        _dbContext.Remove(tag);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+        return 0;
+    }
+}
