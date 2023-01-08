e6d0f478c7e224a766671d3e9c90f44c8da6fe50
Edi Wang <Edi.Wang@outlook.com>
2023-01-08 15:56:12 +0800

    remove tags input

--- src/Admin/elf-admin/src/app/links/edit-link/edit-link-dialog.ts
+++ src/Admin/elf-admin/src/app/links/edit-link/edit-link-dialog.ts
@@ -1,10 +1,6 @@
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 import { Link } from "../link.service";
-import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { MatChipInputEvent } from '@angular/material/chips';
-import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
-import { map, Observable, startWith } from "rxjs";
 import { AppCacheService } from "../../shared/appcache.service";
 import { ValidationErrorMessage } from "src/app/shared/global";
 @Component({
@@ -15,35 +11,21 @@ import { ValidationErrorMessage } from "src/app/shared/global";
 export class EditLinkDialog {
     isBusy = false;
     addOnBlur = true;
-    readonly separatorKeysCodes = [ENTER, COMMA] as const;
-    tagCtrl = new FormControl();
-    filteredTags: Observable<string[]>;
     editLinkForm: FormGroup;
-    tags: string[] = [];
     allTags: string[] = [];
     public active = false;
     @Output() cancel: EventEmitter<any> = new EventEmitter();
     @Output() save: EventEmitter<any> = new EventEmitter();
     public validationErrorMessage = ValidationErrorMessage;
 
-    @ViewChild('tagInput') tagInput: ElementRef<HTMLInputElement>;
-
     constructor(
         public fb: FormBuilder,
         private appCache: AppCacheService) {
-        this.filteredTags = this.tagCtrl.valueChanges.pipe(
-            startWith(null),
-            map((tag: string | null) => (tag ? this._filter(tag) : this.allTags.slice())),
-        );
     }
 
     @Input() public set model(data: Link) {
         this.editLinkForm?.reset(data);
         this.active = data !== undefined;
-
-        if (data) {
-            this.tags = data?.tags?.map(t => t.name);
-        }
     }
 
     ngOnInit(): void {
@@ -65,9 +47,6 @@ export class EditLinkDialog {
         this.allTags = this.appCache.tags.map(t => t.name)
     }
 
-    // submitForm() {
-    //     this.isBusy = true;
-
     //     if (this.data) {
     //         this.linkService
     //             .update(this.data.id, {
@@ -82,56 +61,6 @@ export class EditLinkDialog {
     //                 this.isBusy = false;
     //             });
     //     }
-    //     else {
-    //         this.linkService
-    //             .add({
-    //                 originUrl: this.editLinkForm.value.originUrl.trim(),
-    //                 note: this.editLinkForm.value.note,
-    //                 akaName: this.editLinkForm.value.akaName,
-    //                 isEnabled: this.editLinkForm.value.isEnabled,
-    //                 ttl: this.editLinkForm.value.ttl,
-    //                 tags: this.tags
-    //             })
-    //             .subscribe(() => {
-    //                 this.isBusy = false;
-    //             });
-    //     }
-    // }
-
-    add(event: MatChipInputEvent): void {
-        const value = (event.value || '').trim();
-
-        if (value && !(this.tags.includes(value))) {
-            this.tags.push(value);
-        }
-
-        event.chipInput!.clear();
-        this.tagCtrl.setValue(null);
-    }
-
-    remove(tag: string): void {
-        const index = this.tags.indexOf(tag);
-
-        if (index >= 0) {
-            this.tags.splice(index, 1);
-        }
-    }
-
-    selected(event: MatAutocompleteSelectedEvent): void {
-        console.info(this.tags);
-
-        if (!(this.tags.includes(event.option.viewValue))) {
-            this.tags.push(event.option.viewValue);
-        }
-
-        this.tagInput.nativeElement.value = '';
-        this.tagCtrl.setValue(null);
-    }
-
-    private _filter(value: string): string[] {
-        const filterValue = value.toLowerCase();
-        return this.allTags.filter(tag => tag.toLowerCase().includes(filterValue));
-    }
 
     public onSave(e) {
         e.preventDefault();
