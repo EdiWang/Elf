53a2f33f8b865de46f97181f2c16d95ac9bee00a
Edi Wang <Edi.Wang@outlook.com>
2023-01-08 15:57:33 +0800

    Update app.module.ts

--- src/Admin/elf-admin/src/app/app.module.ts
+++ src/Admin/elf-admin/src/app/app.module.ts
@@ -6,11 +6,6 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatChipsModule } from '@angular/material/chips';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-
 import { HomeComponent } from './home/home.component';
 import { LinkService } from './links/link.service';
 import { LinksComponent } from './links/links.component';
@@ -19,21 +14,12 @@ import { EditTagDialog } from './tag/edit-tag/edit-tag-dialog';
 import { ReportComponent } from './report/report.component';
 import { TagsComponent } from './tag/tags.component';
 
-import { ElfApplicationinsightsErrorHandlerService } from './error-handler.service';
-
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-
-//#region BEGIN MSAL
-
-import { IPublicClientApplication, PublicClientApplication, InteractionType } from '@azure/msal-browser';
-import { MsalGuard, MsalInterceptor, MsalBroadcastService, MsalInterceptorConfiguration, MsalModule, MsalService, MSAL_GUARD_CONFIG, MSAL_INSTANCE, MSAL_INTERCEPTOR_CONFIG, MsalGuardConfiguration, MsalRedirectComponent } from '@azure/msal-angular';
-
-import { msalConfig, loginRequest, protectedResources } from './auth-config';
+//#region Kendo UI
+import 'hammerjs';
 import { InputsModule } from '@progress/kendo-angular-inputs';
 import { GridModule } from '@progress/kendo-angular-grid';
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
 import { ChartsModule } from '@progress/kendo-angular-charts';
-import 'hammerjs';
 import { IndicatorsModule } from '@progress/kendo-angular-indicators';
 import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
 import { DialogsModule } from '@progress/kendo-angular-dialog';
@@ -42,9 +28,18 @@ import { RecentRequestsComponent } from './report/recent-requests/recent-request
 import { NotificationModule } from '@progress/kendo-angular-notification';
 import { BarcodesModule } from '@progress/kendo-angular-barcodes';
 import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+//#endregion
 
+import { ElfApplicationinsightsErrorHandlerService } from './error-handler.service';
 
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
+//#region MSAL
+
+import { IPublicClientApplication, PublicClientApplication, InteractionType } from '@azure/msal-browser';
+import { MsalGuard, MsalInterceptor, MsalBroadcastService, MsalInterceptorConfiguration, MsalModule, MsalService, MSAL_GUARD_CONFIG, MSAL_INSTANCE, MSAL_INTERCEPTOR_CONFIG, MsalGuardConfiguration, MsalRedirectComponent } from '@azure/msal-angular';
+
+import { msalConfig, loginRequest, protectedResources } from './auth-config';
 
 export function MSALInstanceFactory(): IPublicClientApplication {
   return new PublicClientApplication(msalConfig);
@@ -89,10 +84,6 @@ export function MSALGuardConfigFactory(): MsalGuardConfiguration {
     BrowserAnimationsModule,
     HttpClientModule,
     MsalModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatChipsModule,
-    MatAutocompleteModule,
     FormsModule,
     ReactiveFormsModule,
     InputsModule,
