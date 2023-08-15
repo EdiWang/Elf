5846e7368501b27b2a7e131417eb9671711c737b
Edi Wang <Edi.Wang@outlook.com>
2023-08-15 10:13:57 +0800

    trigger build

--- src/Admin/elf-admin/src/environments/environment.ts
+++ src/Admin/elf-admin/src/environments/environment.ts
@@ -10,12 +10,3 @@ export const environment = {
   applicationIdUri: window["env"]["applicationIdUri"] || "api://elf",
   applicationInsightKey: window["env"]["elfApiBaseUrl"] || '00000000-0000-0000-0000-000000000000'
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
