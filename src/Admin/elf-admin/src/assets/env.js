b3ff45817c8d125a01ad607293931669405b2747
Edi Wang <Edi.Wang@outlook.com>
2023-10-28 14:41:22 +0800

    update clientId

--- src/Admin/elf-admin/src/assets/env.js
+++ src/Admin/elf-admin/src/assets/env.js
@@ -2,7 +2,7 @@
     window["env"] = window["env"] || {};
 
     window["env"]["elfApiBaseUrl"] = "https://go.edi.wang";
-    window["env"]["clientId"] = "ecfbb89d-9f5a-4914-939a-bf4800630581";
+    window["env"]["clientId"] = "1c1338b1-50f1-4812-b5a6-c7394b5245c4";
     window["env"]["tenantId"] = "1f4e3112-81fc-4f46-81fd-5c37ac107e26";
     window["env"]["applicationIdUri"] = "api://a439e578-3ff8-4bee-91e5-96141234bc67";
 })(this);
