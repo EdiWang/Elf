fcb20d97d17ee2785ba68d1641a21b465f3fe3c7
Edi Wang <ediwang@greendotcorp.com>
2023-10-30 12:39:56 +0800

    layout update

---
+++ src/Admin/ElfAdmin/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,10 @@
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.Fast.Components.FluentUI;
+
+namespace ElfAdmin.Components.Layout;
+
+public partial class MainLayout : LayoutComponentBase
+{ 
+    ElementReference container;
+}
