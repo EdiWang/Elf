fd0555abf9a9e84d8bbdfee0586f41db225a7620
Edi Wang <Edi.Wang@outlook.com>
2023-10-30 12:03:12 +0800

    empty blazor project

---
+++ src/Admin/ElfAdmin/Program.cs
@@ -0,0 +1,22 @@
+using ElfAdmin.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("ElfAdmin.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ElfAdmin.ServerAPI"));
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("api://a439e578-3ff8-4bee-91e5-96141234bc67/access_as_user");
+});
+
+await builder.Build().RunAsync();
