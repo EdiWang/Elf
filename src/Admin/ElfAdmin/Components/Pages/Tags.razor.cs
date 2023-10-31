46d2366783db872656b0018888971b2d183dd88d
Edi Wang <Edi.Wang@outlook.com>
2023-10-31 20:35:40 +0800

    Update Tags.razor.cs

--- src/Admin/ElfAdmin/Components/Pages/Tags.razor.cs
+++ src/Admin/ElfAdmin/Components/Pages/Tags.razor.cs
@@ -1,10 +1,30 @@
 
 using Microsoft.AspNetCore.Components;
+using Microsoft.Fast.Components.FluentUI;
 
 namespace ElfAdmin.Components.Pages;
 
 public partial class Tags
 {
+    public bool IsBusy { get; set; }
+
     [Inject]
     public HttpClient Http { get; set; }
+
+    [Inject]
+    public IMessageService MessageService { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    { 
+        await GetData();
+    }
+
+    private async Task GetData()
+    {
+        IsBusy = true;
+
+        
+
+        IsBusy = false;
+    }
 }
