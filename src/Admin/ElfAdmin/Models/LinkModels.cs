c8c00e79d7a8c76f3b787dd3bdccd69ddb28bdd5
Edi Wang <Edi.Wang@outlook.com>
2023-10-31 13:09:18 +0800

    ListByTagsRequest

--- src/Admin/ElfAdmin/Models/LinkModels.cs
+++ src/Admin/ElfAdmin/Models/LinkModels.cs
@@ -63,3 +63,16 @@ public class LinkEditModel
 
     public string[] Tags { get; set; }
 }
+
+public class ListByTagsRequest
+{
+    [Required]
+    [MinLength(1)]
+    public int[] TagIds { get; set; }
+
+    [Range(1, int.MaxValue)]
+    public int Take { get; set; }
+
+    [Range(0, int.MaxValue)]
+    public int Offset { get; set; }
+}
