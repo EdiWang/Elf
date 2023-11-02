c0eb1b10103fb7f405b12b26da7bee58f1ee0702
Edi Wang <Edi.Wang@outlook.com>
2023-11-02 10:56:44 +0800

    remove angular app

--- src/Admin/elf-admin/src/app/shared/global.ts
+++
@@ -1,3 +0,0 @@
-export const ValidationErrorMessage = {
-    required: 'Required field'
-}
--- src/Admin/elf-admin/src/assets/env.template.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-    window["env"] = window["env"] || {};
-
-    window["env"]["elfApiBaseUrl"] = "${ELF_API_BASE_URL}";
-    window["env"]["clientId"] = "${CLIENT_ID}";
-    window["env"]["tenantId"] = "${TENANT_ID}";
-    window["env"]["applicationIdUri"] = "${APPLICATION_ID_URI}";
-})(this);
