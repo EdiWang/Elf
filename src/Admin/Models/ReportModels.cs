e4e7d0f6556bb9398b79cb811962033b35886352
Edi Wang <Edi.Wang@outlook.com>
2023-11-15 16:06:58 +0800

    move MostRequestedLinkCount

--- src/Admin/Models/ReportModels.cs
+++ src/Admin/Models/ReportModels.cs
@@ -1,12 +1,5 @@
 namespace ElfAdmin.Models;
 
-public class MostRequestedLinkCount
-{
-    public string FwToken { get; set; }
-    public string Note { get; set; }
-    public int RequestCount { get; set; }
-}
-
 public class LinkTrackingDateCount
 {
     public int RequestCount { get; set; }
