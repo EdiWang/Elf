deaa71c53fcff1633ae0673f10ef77f1902d77aa
Edi Wang <Edi.Wang@outlook.com>
2025-08-27 08:08:05 +0800

    Add EF Core entities and DbContext for link management
    Introduces LinkEntity, TagEntity, LinkTagEntity, and LinkTrackingEntity classes with configurations for Entity Framework Core. Adds ElfDbContext to manage these entities and updates the project file to include EF Core dependencies. Also removes nullable from RequestId in ErrorModel.

---
+++ src/Elf.Admin/Data/LinkEntity.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Text.Json.Serialization;
+
+namespace Elf.Admin.Data;
+
+public class LinkEntity
+{
+    public LinkEntity()
+    {
+        LinkTrackings = new HashSet<LinkTrackingEntity>();
+        Tags = new HashSet<TagEntity>();
+    }
+
+    public int Id { get; set; } // int
+    public string OriginUrl { get; set; } // nvarchar(256)
+    public string FwToken { get; set; } // varchar(32)
+    public string Note { get; set; } // nvarchar(max)
+    public bool IsEnabled { get; set; } // bit
+    public DateTime UpdateTimeUtc { get; set; } // datetime
+    public string AkaName { get; set; } // varchar(32)
+    public int? TTL { get; set; } // int
+
+    [JsonIgnore]
+    public virtual ICollection<LinkTrackingEntity> LinkTrackings { get; set; }
+
+    [JsonIgnore]
+    public virtual ICollection<TagEntity> Tags { get; set; }
+}
+
+internal class LinkEntityConfiguration : IEntityTypeConfiguration<LinkEntity>
+{
+    public void Configure(EntityTypeBuilder<LinkEntity> builder)
+    {
+        builder.Property(e => e.OriginUrl).HasMaxLength(256);
+        builder.Property(e => e.FwToken).HasMaxLength(32);
+        builder.Property(e => e.AkaName).HasMaxLength(32);
+        builder.Property(e => e.UpdateTimeUtc).HasColumnType("datetime");
+    }
+}
---
+++ src/Elf.Admin/Data/TagEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Elf.Admin.Data;
+
+public class TagEntity
+{
+    public TagEntity()
+    {
+        Links = new HashSet<LinkEntity>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [MaxLength(32)]
+    public string Name { get; set; }
+
+    [JsonIgnore]
+    public virtual ICollection<LinkEntity> Links { get; set; }
+}
