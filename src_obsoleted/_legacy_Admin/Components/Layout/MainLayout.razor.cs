6cddedeb3692949bb4b355e95f4b16d56c8cb8ec
Edi Wang <Edi.Wang@outlook.com>
2025-09-01 13:17:39 +0800

    Remove legacy admin Blazor project
    Deleted the entire src_obsoleted/_legacy_Admin directory, including all components, pages, models, utilities, and configuration files. This removes the obsolete Blazor admin implementation and related resources.

--- src_obsoleted/_legacy_Admin/Components/Layout/MainLayout.razor.cs
+++
@@ -1,9 +0,0 @@
-
-using Microsoft.AspNetCore.Components;
-
-namespace ElfAdmin.Components.Layout;
-
-public partial class MainLayout : LayoutComponentBase
-{
-    ElementReference container;
-}
--- src_obsoleted/_legacy_Admin/Constants.cs
+++
@@ -1,7 +0,0 @@
-namespace ElfAdmin;
-
-public static class Constants
-{
-    public static string APIAddress = "https://go.edi.wang";
-    public static string APIAppUrlGuid = "a439e578-3ff8-4bee-91e5-96141234bc67";
-}
